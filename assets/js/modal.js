510a56e43b36d74df395233bdc88158f467fb92a
Taha Zaheer Khan <31067691+musaafir101@users.noreply.github.com>
2025-02-19 21:32:36 +0500

    AU Page UI Completed!

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,15 +2,21 @@
 var modal = document.getElementsByClassName("modal")[0];
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("open-modal")[0];
+var buttons = document.querySelectorAll(".open-modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close-modal")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+});
+// btn.onclick = function () {
+//   console.log("btn", btn);
+//   modal.style.display = "block";
+// };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
